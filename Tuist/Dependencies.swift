2143053ca5d15df1a0e7579cd8faddf345d98d35
Pedro Piñera <pedro@pepicrft.me>
2023-06-29 20:57:41 +0200

    Some small changes

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,5 +3,5 @@ import ProjectDescription
 let dependencies = Dependencies(swiftPackageManager: [
     .package(url: "https://github.com/vapor/vapor.git", from: "4.77.0"),
     .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "1.6.0"),
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2")
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
 ])
